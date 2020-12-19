8927d0a41d9435ff2e00ca558fab817a4195101b
Anna Leontieva <leontieva1anna@gmail.com>
2020-12-20 01:56:47 +0200

    add type

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
+import { Key } from "readline";
 import Affairs from "./Affairs";
 
 // types
-export type AffairPriorityType = any; // need to fix any
-export type AffairType = any; // need to fix any
+export type AffairPriorityType = "high" |"low"|"middle"; // need to fix any
+export type AffairType =any// need to fix any
 export type FilterType = "all" | AffairPriorityType;
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs:any = [ // need to fix any
     {_id: 1, name: "React", priority: "high"},
     {_id: 2, name: "anime", priority: "low"},
     {_id: 3, name: "games", priority: "low"},
@@ -16,16 +17,16 @@ const defaultAffairs: any = [ // need to fix any
 ];
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: any, filter:string):any => { // need to fix any
     if (filter === "all") return affairs;
     else return; // need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
+export const deleteAffair = (affairs: any, _id:number): any => { // need to fix any
     return; // need to fix
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs); // need to fix any
+    const [affairs, setAffairs] = useState<FilterType>(defaultAffairs); 
     const [filter, setFilter] = useState<FilterType>("all");
 
     const filteredAffairs = filterAffairs(affairs, filter);
